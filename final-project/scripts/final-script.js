8d9d740fc759c708e7fed4fda063fb0a068b1d48
allenjandrew <allenjandrew@gmail.com>
2023-02-10 14:13:55 -0700

    More work on final project

--- final-project/scripts/final-script.js
+++ final-project/scripts/final-script.js
@@ -3,90 +3,101 @@
 // - check for collisions
 // - (draw the screen)
 
-// Choose random characters
-const availableChars = ["sans", "megaman", "ninja", "creeper", "dartmonkey"];
-const firstChar = availableChars.splice(
-  Math.floor(Math.random() * availableChars.length),
-  1
-);
-const secondChar = availableChars.splice(
-  Math.floor(Math.random() * availableChars.length),
-  1
-);
-
-// Get char1's position
-const char1 = document.querySelector("#char1");
-char1.src = "images/" + firstChar + "-char.png";
-var rect1 = char1.getBoundingClientRect();
-// console.log(rect1.top, rect1.right, rect1.bottom, rect1.left);
-
-// Get char2's position
-const char2 = document.querySelector("#char2");
-char2.src = "images/" + secondChar + "-char.png";
-var rect2 = char2.getBoundingClientRect();
-
-// Starting frisbee coordinates
-const frisbeeEl = document.querySelector("#frisbee");
-var leftBoundX = rect1.left + 100;
-var startingY = rect1.top + 100;
-frisbeeEl.style.left = leftBoundX + "px";
-frisbeeEl.style.top = startingY + "px";
-
-var rightBoundX;
+var leftChar;
+var rightChar;
+var toRightGlobal = true;
+var leftFrisbeeStop;
+var rightFrisbeeStop;
 var currentX;
-var goalX;
-var toRight = true;
-var timer;
 
-function moveFrisbee(direction) {
-  if (direction == "right") {
+// Choose random characters
+function loadNewChars() {
+  // Set chars list
+  const availableChars = ["sans", "megaman", "ninja", "creeper", "dartmonkey"];
+
+  // Randomly pick char 1
+  leftChar = availableChars.splice(
+    Math.floor(Math.random() * availableChars.length),
+    1
+  );
+  // Randomly pick char 2
+  rightChar = availableChars.splice(
+    Math.floor(Math.random() * availableChars.length),
+    1
+  );
+
+  // Set char 1 image
+  let char1 = document.querySelector("#char1");
+  char1.src = `images/${leftChar}-char.png`;
+
+  // Set char 2 image
+  let char2 = document.querySelector("#char2");
+  char2.src = `images/${rightChar}-char.png`;
+
+  // Starting frisbee coordinates
+  const frisbeeEl = document.querySelector("#frisbee");
+  frisbeeEl.style.left =
+    char1.getBoundingClientRect().right - char1.width / 3 + "px";
+  frisbeeEl.style.top = char1.getBoundingClientRect().top + 100 + "px";
+
+  toRightGlobal = true;
+  document.activeElement.blur();
+
+  leftFrisbeeStop = char1.getBoundingClientRect().right - char1.width / 3;
+  console.log(
+    char1.getBoundingClientRect().right,
+    char1.width,
+    char1.width / 3,
+    leftFrisbeeStop,
+    frisbeeEl.width
+  );
+  rightFrisbeeStop =
+    char2.getBoundingClientRect().left + char2.width / 3 - frisbeeEl.width;
+}
+
+function moveFrisbee(toRight) {
+  if (toRight) {
     currentX += 1;
   } else {
     currentX -= 1;
   }
-  frisbeeEl.style.left = currentX + "px";
+  document.querySelector("#frisbee").style.left = currentX + "px";
 }
 
-function throwFrisbeeRight() {
-  rightBoundX = rect2.left - char2.width;
+function throwFrisbee(toRight) {
+  // Set to throwing sprite
+  let charEl;
+  let charName;
 
-  currentX = leftBoundX;
-  char1.src = "images/" + firstChar + "-throw.png";
+  if (toRight) {
+    charEl = document.querySelector("#char1");
+    charName = leftChar;
+    currentX = leftFrisbeeStop;
+  } else {
+    charEl = document.querySelector("#char2");
+    charName = rightChar;
+    currentX = rightFrisbeeStop;
+  }
+  charEl.src = `images/${charName}-throw.png`;
 
-  timer = 0;
-  for (i = 0; i < rightBoundX - leftBoundX; i++) {
-    setTimeout(moveFrisbee, timer, "right");
+  // Move frisbee
+  let timer = 0;
+  for (i = 0; i < rightFrisbeeStop - leftFrisbeeStop; i++) {
+    setTimeout(moveFrisbee, timer, toRight);
     timer += 1;
   }
 
+  // Set back to standing sprite
   setTimeout(() => {
-    char1.src = "images/" + firstChar + "-char.png";
+    charEl.src = `images/${charName}-char.png`;
   }, 350);
-  toRight = false;
-}
-
-function throwFrisbeeLeft() {
-  rightBoundX = rect2.left - char2.width;
-
-  currentX = rightBoundX;
-  char2.src = "images/" + secondChar + "-throw.png";
-
-  timer = 0;
-  for (i = 0; i < rightBoundX - leftBoundX; i++) {
-    setTimeout(moveFrisbee, timer, "left");
-    timer += 1;
-  }
-
-  setTimeout(() => {
-    char2.src = "images/" + secondChar + "-char.png";
-  }, 350);
-  toRight = true;
 }
 
 document.addEventListener("keypress", (event) => {
-  if (toRight) {
-    throwFrisbeeRight();
-  } else {
-    throwFrisbeeLeft();
-  }
+  throwFrisbee((toRight = toRightGlobal));
+  toRightGlobal = !toRightGlobal;
 });
+
+document.querySelector("#new-chars").addEventListener("click", loadNewChars);
+
+setTimeout(loadNewChars, 1);
