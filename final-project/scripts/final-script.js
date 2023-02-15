3f0dd5d12d81f93cb0d8b1aafef96a88a0f6915a
allenjandrew <allenjandrew@gmail.com>
2023-02-14 20:28:47 -0700

    Finished final project! Still a couple bugs tho

--- final-project/scripts/final-script.js
+++ final-project/scripts/final-script.js
@@ -3,14 +3,22 @@
 // - check for collisions
 // - (draw the screen)
 
+// Global variables
 var leftChar;
 var rightChar;
-var toRightGlobal = true;
+var toRightBool = true;
+var thrownBool = false;
+var playableBool = true;
 var leftFrisbeeStop;
 var rightFrisbeeStop;
+var frisbeeBaseY;
 var currentX;
+var currentY;
+var timeouts = [];
+var scoreCounter = 0;
+var highScore = 0;
 
-// Choose random characters
+// Function definitions
 function loadNewChars() {
   // Set chars list
   const availableChars = ["sans", "megaman", "ninja", "creeper", "dartmonkey"];
@@ -29,46 +37,101 @@ function loadNewChars() {
   // Set char 1 image
   let char1 = document.querySelector("#char1");
   char1.src = `images/${leftChar}-char.png`;
+  char1.alt = `Character 1 - ${leftChar}`;
 
   // Set char 2 image
   let char2 = document.querySelector("#char2");
   char2.src = `images/${rightChar}-char.png`;
+  char2.alt = `Character 2 - ${rightChar}`;
 
-  // Starting frisbee coordinates
   const frisbeeEl = document.querySelector("#frisbee");
-  frisbeeEl.style.left =
-    char1.getBoundingClientRect().right - char1.width / 3 + "px";
-  frisbeeEl.style.top = char1.getBoundingClientRect().top + 100 + "px";
-
-  toRightGlobal = true;
-  document.activeElement.blur();
 
   leftFrisbeeStop = char1.getBoundingClientRect().right - char1.width / 3;
-  console.log(
-    char1.getBoundingClientRect().right,
-    char1.width,
-    char1.width / 3,
-    leftFrisbeeStop,
-    frisbeeEl.width
-  );
   rightFrisbeeStop =
     char2.getBoundingClientRect().left + char2.width / 3 - frisbeeEl.width;
+  frisbeeBaseY = char1.getBoundingClientRect().top + 100;
+
+  resetFrisbee();
+
+  toRightBool = true;
+  thrownBool = false;
+  document.activeElement.blur();
+}
+
+function loadNewStage() {
+  const availableStages = [
+    "battlefield",
+    "cityair",
+    "citynight",
+    "citypink",
+    "deserttree",
+    "rundown",
+    "spaceearth",
+  ];
+
+  stage = availableStages.splice(
+    Math.floor(Math.random() * availableStages.length),
+    1
+  );
+
+  backgroundEl = document.querySelector("#background-img");
+  backgroundEl.src = `images/${stage}-stage.jpg`;
+  backgroundEl.alt = `background stage - ${stage}`;
+
+  document.activeElement.blur();
+}
+
+function resetFrisbee() {
+  const frisbeeEl = document.querySelector("#frisbee");
+  frisbeeEl.style.left = leftFrisbeeStop + "px";
+  frisbeeEl.style.top = frisbeeBaseY + "px";
 }
 
 function moveFrisbee(toRight) {
+  let speed = 1 + scoreCounter / 25;
+
   if (toRight) {
-    currentX += 1;
+    currentX += speed;
   } else {
-    currentX -= 1;
+    currentX -= speed;
   }
-  document.querySelector("#frisbee").style.left = currentX + "px";
+
+  // console.log(currentX, leftFrisbeeStop, rightFrisbeeStop, currentY);
+  if (currentX - leftFrisbeeStop < (rightFrisbeeStop - leftFrisbeeStop) / 3) {
+    if (toRight) {
+      currentY -= 1;
+    } else {
+      currentY += 1;
+    }
+  } else if (
+    currentX - leftFrisbeeStop >
+    ((rightFrisbeeStop - leftFrisbeeStop) * 2) / 3
+  ) {
+    if (toRight) {
+      currentY += 1;
+    } else {
+      currentY -= 1;
+    }
+  }
+
+  if (currentY > frisbeeBaseY + 90) {
+    currentY = frisbeeBaseY + 90;
+  }
+
+  const frisbeeEl = document.querySelector("#frisbee");
+  frisbeeEl.style.left = currentX + "px";
+  frisbeeEl.style.top = currentY + "px";
+
+  if (window.innerWidth - currentX < frisbeeEl.width || currentX < 1) {
+    youLose();
+  }
 }
 
 function throwFrisbee(toRight) {
-  // Set to throwing sprite
   let charEl;
   let charName;
 
+  // Set to throwing sprite
   if (toRight) {
     charEl = document.querySelector("#char1");
     charName = leftChar;
@@ -80,24 +143,90 @@ function throwFrisbee(toRight) {
   }
   charEl.src = `images/${charName}-throw.png`;
 
-  // Move frisbee
+  currentY = frisbeeBaseY;
+
+  // Move frisbee with timeout
   let timer = 0;
-  for (i = 0; i < rightFrisbeeStop - leftFrisbeeStop; i++) {
-    setTimeout(moveFrisbee, timer, toRight);
-    timer += 1;
+  console.log(window.innerWidth, window.outerWidth, rightFrisbeeStop);
+  for (i = 0; i < window.innerWidth; i++) {
+    timeouts.push(setTimeout(moveFrisbee, timer, toRight));
+    timer += 2;
   }
 
-  // Set back to standing sprite
+  // Set back to standing sprite after timeout
   setTimeout(() => {
     charEl.src = `images/${charName}-char.png`;
   }, 350);
+
+  // Hide intro box
+  document.querySelector("#intro-box").classList.add("hidden");
 }
 
-document.addEventListener("keypress", (event) => {
-  throwFrisbee((toRight = toRightGlobal));
-  toRightGlobal = !toRightGlobal;
+function catchFrisbee(toRight) {
+  if (toRight) {
+    stopper = rightFrisbeeStop;
+  } else {
+    stopper = leftFrisbeeStop;
+  }
+
+  if (Math.abs(currentX - stopper - 25) <= 75) {
+    timeouts.forEach((timerID) => {
+      clearTimeout(timerID);
+    });
+    timeouts = [];
+
+    document.querySelector("#frisbee").style.left = stopper + "px";
+    document.querySelector("#frisbee").style.top = frisbeeBaseY + "px";
+    scoreCounter++;
+  } else {
+    youLose();
+  }
+}
+
+function youLose() {
+  if (scoreCounter > highScore) {
+    highScore = scoreCounter;
+  }
+  if (playableBool) {
+    document.querySelector("#you-lose").classList.remove("hidden");
+    document.querySelector("#intro-box").classList.remove("hidden");
+    document.querySelector(
+      "#score-count"
+    ).innerHTML = `You suck!</br>Score: ${scoreCounter}</br>High Score: ${highScore}`;
+
+    scoreCounter = 0;
+    playableBool = false;
+  }
+}
+
+// Event listeners
+document.addEventListener("keypress", () => {
+  if (playableBool) {
+    if (thrownBool) {
+      catchFrisbee(toRightBool);
+      toRightBool = !toRightBool;
+      thrownBool = !thrownBool;
+    } else {
+      throwFrisbee((toRight = toRightBool));
+      thrownBool = !thrownBool;
+    }
+  }
 });
 
 document.querySelector("#new-chars").addEventListener("click", loadNewChars);
 
-setTimeout(loadNewChars, 1);
+document.querySelector("#reset-button").addEventListener("click", () => {
+  document.querySelector("#you-lose").classList.add("hidden");
+  resetFrisbee();
+  toRightBool = true;
+  thrownBool = false;
+  playableBool = true;
+});
+
+document.querySelector("#new-stage").addEventListener("click", loadNewStage);
+
+// Run on setup
+window.addEventListener("load", () => {
+  loadNewChars();
+  loadNewStage();
+});
